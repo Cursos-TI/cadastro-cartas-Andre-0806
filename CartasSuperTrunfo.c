ff0f400d107853d543dfddf4baed39cfaa49296e
Andre-0806 <146041870+Andre-0806@users.noreply.github.com>
2026-02-26 01:54:19 -0300

    Display information for four Brazilian cities

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main() { 
+    int populacao, pontos_turisticos;
+    float area, pib;
+    char Cidade1[50], Cidade2[50], Cidade3[50], Cidade4[50];
+    // Cidade 1
+    printf("Rio de Janeiro\n");
+    printf("DDD: 21\n");
+    printf("Pontos turisticos: Cristo Redentor, Pao de Acucar, Copacabana\n");
+    printf("Populacao: 6.7 milhoes\n");
+    printf("Area: 1.200 km²\n");
+    printf("PIB: R$ 300 bilhoes\n\n");
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    // Cidade 2
+    printf("Sao Paulo\n");
+    printf("DDD: 11\n");
+    printf("Pontos turisticos: Avenida Paulista, Parque Ibirapuera, Mercado Municipal\n");
+    printf("Populacao: 12.3 milhoes\n");
+    printf("Area: 1.521 km²\n");
+    printf("PIB: R$ 700 bilhoes\n\n");
 
-  // Área para entrada de dados
+    // Cidade 3
+    printf("Salvador\n");
+    printf("DDD: 71\n");
+    printf("Pontos turisticos: Pelourinho, Elevador Lacerda, Praia do Forte\n");
+    printf("Populacao: 2.9 milhoes\n");
+    printf("Area: 706 km²\n");
+    printf("PIB: R$ 150 bilhoes\n\n");
 
-  // Área para exibição dos dados da cidade
-
+    // Cidade 4
+    printf("Brasilia\n");
+    printf("DDD: 61\n");
+    printf("Pontos turisticos: Congresso Nacional, Catedral Metropolitana, Palácio da Alvorada\n");
+    printf("Populacao: 3 milhoes\n");
+    printf("Area: 5.802 km²\n");
+    printf("PIB: R$ 250 bilhoes\n\n"); 
+    
 return 0;
-} 
+
+}
