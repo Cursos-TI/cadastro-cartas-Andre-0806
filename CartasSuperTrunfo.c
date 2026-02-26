6cb7486b7ec93da6e8280fd469d94ba6557c909a
Andre-0806 <146041870+Andre-0806@users.noreply.github.com>
2026-02-26 02:14:03 -0300

    Brasil

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() { 
-    int populacao, pontos_turisticos;
+    int populacao, pontos_turisticos, ddd;
     float area, pib;
     char Cidade1[50], Cidade2[50], Cidade3[50], Cidade4[50];
+    
     // Cidade 1
-    printf("Rio de Janeiro\n");
+    printf("--Rio de Janeiro--\n");
     printf("DDD: 21\n");
     printf("Pontos turisticos: Cristo Redentor, Pao de Acucar, Copacabana\n");
     printf("Populacao: 6.7 milhoes\n");
@@ -13,7 +14,7 @@ int main() {
     printf("PIB: R$ 300 bilhoes\n\n");
 
     // Cidade 2
-    printf("Sao Paulo\n");
+    printf("--Sao Paulo--\n");
     printf("DDD: 11\n");
     printf("Pontos turisticos: Avenida Paulista, Parque Ibirapuera, Mercado Municipal\n");
     printf("Populacao: 12.3 milhoes\n");
@@ -21,7 +22,7 @@ int main() {
     printf("PIB: R$ 700 bilhoes\n\n");
 
     // Cidade 3
-    printf("Salvador\n");
+    printf("--Salvador--\n");
     printf("DDD: 71\n");
     printf("Pontos turisticos: Pelourinho, Elevador Lacerda, Praia do Forte\n");
     printf("Populacao: 2.9 milhoes\n");
@@ -29,7 +30,7 @@ int main() {
     printf("PIB: R$ 150 bilhoes\n\n");
 
     // Cidade 4
-    printf("Brasilia\n");
+    printf("--Brasilia--\n");
     printf("DDD: 61\n");
     printf("Pontos turisticos: Congresso Nacional, Catedral Metropolitana, Palácio da Alvorada\n");
     printf("Populacao: 3 milhoes\n");
